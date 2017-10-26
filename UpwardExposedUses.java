57c3c46d664cfdcacf50f9d868b44054a4283358
biikayy <bkama94@gmail.com>
2017-10-26 18:33:10 -0400

    Add files via upload

--- UpwardExposedUses.java
+++ UpwardExposedUses.java
@@ -1,28 +1,42 @@
 package csc410.hw3;
 
+import java.util.*;
+
+import soot.Local;
+import soot.Unit;
+import soot.ValueBox;
+import soot.toolkits.graph.DirectedGraph;
+import soot.toolkits.scalar.BackwardFlowAnalysis;
+import soot.toolkits.scalar.FlowSet;
+import soot.toolkits.scalar.ArraySparseSet;
+
+
 /*** Class to hold Upward Exposed Uses analyis. Extend appropriate class. ***/
 
 // Upward Exposed Uses is a backward flow analysis and will extend such class
-public class UpwardExposedUses extends BackwardsFlowAnalysis {
+public class UpwardExposedUses extends BackwardFlowAnalysis<Unit, FlowSet<Local>>  {
 	
+	private FlowSet<Local> emptySet;
+
 	// Constructor for class
 	public UpwardExposedUses(DirectedGraph g) {
-			super(g);		
-			doAnalysis();
-	}
+			super(g);	
+			emptySet = new ArraySparseSet<Local>();	
+			doAnalysis(); }
 
 	protected FlowSet<Local> newInitialFlow() { return new ArraySparseSet<Local>(); }
 	protected FlowSet<Local> entryInitialFlow() { return new ArraySparseSet<Local>(); }
 
+	@Override
 	protected void merge(FlowSet<Local> in1, FlowSet<Local> in2, FlowSet<Local> out) {	
 	in1.union(in2, out);
 	}
 
-
+	@Override
 	protected void flowThrough(FlowSet<Local> in, Unit node, FlowSet<Local> out) {
 	
 	// New set to keep track of all the writes
-	FlowSet<Local> writes = new ArraySet<Local>();
+	FlowSet<Local> writes = new ArraySparseSet<Local>();
 
 	// 
 	for (ValueBox def: node.getUseAndDefBoxes()) {
@@ -36,14 +50,11 @@ public class UpwardExposedUses extends BackwardsFlowAnalysis {
 	}
 	}
 
+	@Override
 	protected void copy(FlowSet<Local> srcSet, FlowSet<Local> destSet) {
 	srcSet.copy(destSet);
 	}
 
 
-    public static void main(String[] args) {
-		PackManager.v().getPack("wjtp").add(new Transform("wjtp.dfa", new AnalysisTransformer()));
-		soot.Main.main(sootArgs);
-	}
 }
 
